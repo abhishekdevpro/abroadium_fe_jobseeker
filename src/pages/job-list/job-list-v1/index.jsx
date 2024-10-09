42f41842ab9742e654af8a5708cb85a1af189cfb
harshtrivedi01 <harsh2trivedi786@gmail.com>
2024-10-09 19:25:33 +0530

    update

--- src/pages/job-list/job-list-v1/index.jsx
+++ src/pages/job-list/job-list-v1/index.jsx
@@ -4,7 +4,7 @@ import JobList from "@/components/job-listing-pages/job-list-v1";
 import MetaComponent from "@/components/common/MetaComponent";
 
 const metadata = {
-  title: "SenrtySpot - One Stop Career Solution",
+  title: "Abroadium - One Stop Career Solution",
   description: "Abroadium - Job Borad ReactJs Template",
 };
 
